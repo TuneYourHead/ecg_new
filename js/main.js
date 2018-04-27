cec8f9ca2771f5e6f94cab15680044a7d7eba5eb
Tanya <tuneyourhead@gmail.com>
2018-04-27 04:23:43 +0300

    fixed project text

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 $(document).ready(function(){
 
-  $(function() {
-    $.scrollify({
-  		section : ".scrollify",
-  		sectionName : "section-name",
-  		easing: "easeOutExpo",
-      setHeights: false,
-      updateHash: false,
-  		scrollSpeed: 1000,
-  		offset : 0,
-      overflowScroll: true,
-  		before:function() {},
-  		after:function() {}
-  	});
-  });
-
-  var e = ["crea", 300, "tive", 1500, !1, "driven", 800, !1, "modern", 200, 800, !1, "pass", 500, "ionate"],
-    a = $(".header-typed-el").typeIt({
-      speed: 250,
-      autoStart: !1,
-      loop: !0
-      });
-
-  for (var t in e) "string" == typeof e[t] ? a = a.tiType(e[t]) : !1 === e[t] ? a = a.tiDelete() : Number.isInteger(e[t]) && (a = e[t] > 0 ? a.tiPause(e[t]) : a.tiDelete(-e[t]))
-
-  $('.mobile-nav-icon').on('click', function(){
-    $(this).toggleClass('mobile-nav-icon_close');
-    $('.header-nav').toggleClass('header-nav-mobile_active');
-  });
-
   var animateCounter = function(){
     if($('.odometer').visible()){
       $(".odometer").each(function(){
@@ -38,27 +9,69 @@ $(document).ready(function(){
     }
   };
 
-  animateCounter();
-
-  if(window.matchMedia('(max-width: 600px)').matches){
-    $('.team').slick({
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      arrows: false,
-      autoplay: true,
-      responsive: [{
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }]
+  var smoothScroll = function(){
+    $.scrollify({
+      section : ".scrollify",
+      sectionName : "section-name",
+      easing: "easeOutExpo",
+      setHeights: false,
+      updateHash: false,
+      scrollSpeed: 1000,
+      offset : 0,
+      overflowScroll: true,
+      before:function() {},
+      after:function() {}
     });
-  }
+  };
+
+  var activateSlider = function(){
+    if(window.matchMedia('(max-width: 600px)').matches){
+      $('.team').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        autoplay: true,
+        responsive: [{
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      });
+    }
+  };
+
+  var typingAnimation = function(){
+    var e = ["crea", 300, "tive", 1500, !1, "driven", 800, !1, "modern", 200, 800, !1, "pass", 500, "ionate"],
+      a = $(".header-typed-el").typeIt({
+        speed: 250,
+        autoStart: !1,
+        loop: !0
+        });
+    for (var t in e) "string" == typeof e[t] ? a = a.tiType(e[t]) : !1 === e[t] ? a = a.tiDelete() : Number.isInteger(e[t]) && (a = e[t] > 0 ? a.tiPause(e[t]) : a.tiDelete(-e[t]))
+  };
+  
+  typingAnimation();
+  animateCounter();
+  activateSlider();
+  smoothScroll();
 
   $( window ).scroll(function() {
     animateCounter();
   });
+  $(window).resize(function(){
+    activateSlider();
+  });
+
+  $('.mobile-nav-icon').on('click', function(){
+    $(this).toggleClass('mobile-nav-icon_close');
+    $('.header-nav').toggleClass('header-nav-mobile_active');
+  });
+
+  $('.header-scroll-arrow').on('click', function(){
+    $.scrollify.next();
+  })
 
 });
